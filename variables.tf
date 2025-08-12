31ea26dc4c9de15d9f0a7598e1f7b594dbd69623
skr2856 <skr6528@gmail.com>
2025-08-12 21:55:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "region" {
   description = "aws region"
   type = string
-  defult = "us-west-2"
+  default = "us-west-2"
 }
 
 variable "instance_type"{
   description = "type of the ec2-instance"
   type = string
-  defult = "t2.micro"
+  default = "t2.micro"
 }
 
 variable "ami"{
   decription = "ami id of for the instance"
   type = string 
-  defult = "ami-0efdf839508ec2995"  #US-West-2
+  default = "ami-0efdf839508ec2995"  #US-West-2
 }
