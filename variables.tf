c50f79fd7b97edcf269e31715e3d9acb73fc0d29
skr2856 <skr6528@gmail.com>
2025-08-12 21:08:22 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  description = "aws region"
+  type = string
+}
+
+variable "instance_type"{
+  description = "type of the ec2-instance"
+  type = string
+}
+
+variable "ami"{
+  decription = "ami id of for the instance"
+  type = string 
+}
